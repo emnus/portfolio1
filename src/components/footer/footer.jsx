111b8436fdbafd77c9dd01056d593973e050a3a1
emiliano <emiliano.berisiartua29@hotmail.com>
2022-11-02 19:02:41 -0300

    added footer stuf

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function footer (props) {
+  return (
+    <div>
+        <div>
+        <a href="">{prop.credits}</a>
+        <a href="">{prop.aboutsus}</a>
+        </div>
+        <div>
+            <a href="">{props.linkedin}</a>
+            <a href="">{props.whatsapp}</a>
+            <a href="">{props.mail}</a>
+            
+        </div>
+    </div>
+  )
+}
+
+export default footer
